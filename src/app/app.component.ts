810629046cd792d76f1a6fdbf10da413a2a63906
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-06-18 01:58:21 +0800

    hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent],
 })
 export class AppComponent {
   title = 'Restaurant-Finder';
