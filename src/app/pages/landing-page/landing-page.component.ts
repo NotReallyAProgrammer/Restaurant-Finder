ca1199e6018cd6856d84df79cb470eb019ba8895
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-07-12 01:08:14 +0800

    card

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../../layouts/cards/cards.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CardsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
