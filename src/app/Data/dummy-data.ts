9813202f0e827d191401e8c6a2629ea665df64fb
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-07-27 19:20:08 +0800

    data

---
+++ src/app/Data/dummy-data.ts
@@ -0,0 +1,74 @@
+export const restaurantData = [
+  {
+    restaurantName: 'Anchor & James',
+    restaurantType: 'American',
+    restaurantLocation: 'Downtown Union Street',
+    restaurantPricePoint: '$30 and under',
+    restaurantRating: 4.5,
+  },
+
+  {
+    restaurantName: 'Bella Italia',
+    restaurantType: 'Italian',
+    restaurantLocation: 'Maple Street',
+    restaurantPricePoint: '$40 and under',
+    restaurantRating: 4.7,
+  },
+  {
+    restaurantName: 'Sushi Haven',
+    restaurantType: 'Japanese',
+    restaurantLocation: 'Ocean Avenue',
+    restaurantPricePoint: '$50 and under',
+    restaurantRating: 4.6,
+  },
+  {
+    restaurantName: 'Taco Fiesta',
+    restaurantType: 'Mexican',
+    restaurantLocation: 'Elm Street',
+    restaurantPricePoint: '$20 and under',
+    restaurantRating: 4.3,
+  },
+  {
+    restaurantName: 'Spice Symphony',
+    restaurantType: 'Indian',
+    restaurantLocation: 'Pine Street',
+    restaurantPricePoint: '$35 and under',
+    restaurantRating: 4.8,
+  },
+
+  {
+    restaurantName: 'Le Bistro',
+    restaurantType: 'French',
+    restaurantLocation: 'Cedar Boulevard',
+    restaurantPricePoint: '$60 and under',
+    restaurantRating: 4.9,
+  },
+  {
+    restaurantName: 'Dragon Garden',
+    restaurantType: 'Chinese',
+    restaurantLocation: 'Willow Street',
+    restaurantPricePoint: '$25 and under',
+    restaurantRating: 4.4,
+  },
+  {
+    restaurantName: 'Grill Master',
+    restaurantType: 'Barbecue',
+    restaurantLocation: 'Lincoln Avenue',
+    restaurantPricePoint: '$35 and under',
+    restaurantRating: 4.6,
+  },
+  {
+    restaurantName: 'Veggie Delight',
+    restaurantType: 'Vegetarian',
+    restaurantLocation: 'Park Avenue',
+    restaurantPricePoint: '$20 and under',
+    restaurantRating: 4.5,
+  },
+  {
+    restaurantName: 'Tapas & Wine',
+    restaurantType: 'Spanish',
+    restaurantLocation: 'Birch Road',
+    restaurantPricePoint: '$45 and under',
+    restaurantRating: 4.7,
+  },
+];
