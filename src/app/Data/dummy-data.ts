febb25636094b2cad3c24dd402fd262ab7f9c55f
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-09-03 20:33:33 +0800

    .

--- src/app/Data/dummy-data.ts
+++ src/app/Data/dummy-data.ts
@@ -1,6 +1,8 @@
 export const restaurantData = [
   {
-    restaurantImg: '',
+    restaurantImg:
+      'https://images.unsplash.com/photo-1599863489832-99575b1eb286?q=80&w=2057&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
     restaurantName: 'Anchor & James',
     restaurantType: 'American',
     restaurantLocation: 'Downtown Union Street',
@@ -9,6 +11,8 @@ export const restaurantData = [
   },
 
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1629624123501-7595e0193fe0?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Bella Italia',
     restaurantType: 'Italian',
     restaurantLocation: 'Maple Street',
@@ -16,6 +20,8 @@ export const restaurantData = [
     restaurantRating: 4.7,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1651977560863-ca39bfa5176f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Sushi Haven',
     restaurantType: 'Japanese',
     restaurantLocation: 'Ocean Avenue',
@@ -23,6 +29,8 @@ export const restaurantData = [
     restaurantRating: 4.6,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1653796149030-c14703f9a64d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Taco Fiesta',
     restaurantType: 'Mexican',
     restaurantLocation: 'Elm Street',
@@ -30,6 +38,8 @@ export const restaurantData = [
     restaurantRating: 4.3,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1536305030588-45dc07a2a372?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Spice Symphony',
     restaurantType: 'Indian',
     restaurantLocation: 'Pine Street',
@@ -38,6 +48,8 @@ export const restaurantData = [
   },
 
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Le Bistro',
     restaurantType: 'French',
     restaurantLocation: 'Cedar Boulevard',
@@ -47,6 +59,8 @@ export const restaurantData = [
 
   //1
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1591214896508-22fc74d84a75?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Dragon Garden',
     restaurantType: 'Chinese',
     restaurantLocation: 'Willow Street',
@@ -54,6 +68,8 @@ export const restaurantData = [
     restaurantRating: 4.4,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1552566626-52f8b828add9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Grill Master',
     restaurantType: 'Barbecue',
     restaurantLocation: 'Lincoln Avenue',
@@ -61,6 +77,8 @@ export const restaurantData = [
     restaurantRating: 4.6,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1604349899568-1480825fdcd6?q=80&w=1914&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Veggie Delight',
     restaurantType: 'Vegetarian',
     restaurantLocation: 'Park Avenue',
@@ -68,6 +86,8 @@ export const restaurantData = [
     restaurantRating: 4.5,
   },
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1497644083578-611b798c60f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Tapas & Wine',
     restaurantType: 'Spanish',
     restaurantLocation: 'Birch Road',
@@ -76,6 +96,8 @@ export const restaurantData = [
   },
 
   {
+    restaurantImg:
+      'https://images.unsplash.com/photo-1579236546973-39f510ef2a37?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     restaurantName: 'Lola Nena',
     restaurantType: 'Filipino',
     restaurantLocation: 'Birch Road',
